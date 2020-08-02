07c53fd406378a2c6cfe47e02ef0feeaeb294c6a
Rishita Dadhich <rishitarimmi@gmail.com>
2020-08-03 02:57:51 +0530

    completed the 3D card assignment

---
+++ Lecture 11/app.js
@@ -0,0 +1,39 @@
+console.log("Hello!!!!!!!!!!1")
+let n = parseInt(process.argv[20])
+if(process.argv.length<=2){
+    n = 2
+}
+// function fizzbuzz(){
+//     for(let i=0;i<16;i++){
+//         let str =""
+//         if(i%3==0)
+//         str +="Fizz"
+//         if(i%5==0)
+//         str +="Buzz"
+//         if(str =="")
+//         str =i
+//         console.log(str)
+//     }
+// }
+
+function fizzbuzz(){
+    for(let i=0;i<16;i++){
+        let str =""
+        if()
+        str +="Fizz"
+        if(i%5==0)
+        str +="Buzz"
+        if(str =="")
+        str =i
+        console.log(str)
+    }
+}
+
+fizzbuzz()
+
+let obj={
+    a:10,
+    b:"rishita",
+    c: true
+}
+console.log(obj)
